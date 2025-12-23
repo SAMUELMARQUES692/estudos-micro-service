817dafd69694dd1bc0419bf5f676a499604e986f
unknown <samueljks692@gmail.com>
2025-12-23 17:23:36 -0300

    commit samuel

--- samuel-estudos/src/main/java/com/br/app/controller/ClienteController.java
+++ samuel-estudos/src/main/java/com/br/app/controller/ClienteController.java
@@ -8,6 +8,11 @@ import com.br.app.dto.AlunoDTO;
 import com.br.app.service.AlunoService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -18,13 +23,27 @@ public class ClienteController {
 
 	@Autowired
 	private AlunoService alunoService;
-	
-	@GetMapping
+
+	@GetMapping("/alunos")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Sucesso", content = {
+			@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = AlunoDTO.class))) }),
+			@ApiResponse(responseCode = "204", description = "Sem conteudo", content = @Content),
+			@ApiResponse(responseCode = "401", description = "Autênticação é obrigatória", content = @Content),
+			@ApiResponse(responseCode = "403", description = "usuarioAcao é obrigatório", content = @Content),
+			@ApiResponse(responseCode = "500", description = "Erro interno sem causa mapeada.", content = @Content),
+			@ApiResponse(responseCode = "504", description = "Gateway Time-Out", content = @Content) })
 	@Operation(summary = "busca todos os alunos", description = "")
 	public AlunoDTO buscarTodosAluno() {
-		
+
 		return alunoService.buscarTodosAlunos();
 	}
-	
+
+	@GetMapping("/aluno/id")
+	@Operation(summary = "buscar aluno pelo Id", description = "")
+	public AlunoDTO buscarIdAluno() {
+
+		return alunoService.buscarTodosAlunos();
+	}
 
 }
--- samuel-estudos/src/main/java/com/br/app/dto/AlunoDTO.java
+++ samuel-estudos/src/main/java/com/br/app/dto/AlunoDTO.java
@@ -1,14 +1,24 @@
 package com.br.app.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class AlunoDTO {
 
+	@Schema(name= "id", description = "identificador do aluno", example = "123")
 	private Integer id;
+	
+	@Schema(name= "nome", description = "nome do aluno", example = "samuel")
 	private String nome;
+	
+	@Schema(name= "telefone", description = "telefone do aluno", example = "(11) 94949-3483")
 	private String telefone;
+	
+	@Schema(name= "idade", description = "idade do aluno", example = "22")
 	private Integer idade;
+	
+	@Schema(name= "endereco", description = "endereço do aluno", example = "Rua XTPO 123")
 	private String endereco;
 	
 }
